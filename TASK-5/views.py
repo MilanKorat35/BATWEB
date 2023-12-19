64c1fc668c9e17105f640b0b89d3f10a6fb3c621
MilanKorat35 <153885281+MilanKorat35@users.noreply.github.com>
2023-12-19 21:29:20 +0530

    Update views.py

--- TASK-5/views.py
+++ TASK-5/views.py
@@ -1,1 +1,12 @@
+# resumeapp/views.py
+
+from django.shortcuts import render
+
+def resume(request):
+    resume_data = {
+        'name': 'Your Name',
+        'skills': ['Skill 1', 'Skill 2', 'Skill 3'],
+        # ... other resume data
+    }
+    return render(request, 'resumeapp/resume.html', {'resume_data': resume_data})
 
