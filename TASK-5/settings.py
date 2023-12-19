0b549602719487e03fcd67096a8f9d7735a11af6
MilanKorat35 <153885281+MilanKorat35@users.noreply.github.com>
2023-12-19 21:30:41 +0530

    Update settings.py

--- TASK-5/settings.py
+++ TASK-5/settings.py
@@ -1,1 +1,16 @@
+# myresume/settings.py
+
+INSTALLED_APPS = [
+    # ... other apps
+    'resumeapp',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        # ... other settings
+    },
+]
 
