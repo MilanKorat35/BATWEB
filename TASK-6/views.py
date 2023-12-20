8919005133924e1ee40ca49b6c60f3cc3d4c56bc
MilanKorat35 <153885281+MilanKorat35@users.noreply.github.com>
2023-12-20 20:44:48 +0530

    Update views.py

--- TASK-6/views.py
+++ TASK-6/views.py
@@ -1,1 +1,20 @@
+# students/views.py
+from django.shortcuts import render, redirect
+from .forms import StudentForm
+from .models import Student
+
+def student_form(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('student_database')
+    else:
+        form = StudentForm()
+
+    return render(request, 'students/student_form.html', {'form': form})
+
+def student_database(request):
+    students = Student.objects.all()
+    return render(request, 'students/student_database.html', {'students': students})
 
