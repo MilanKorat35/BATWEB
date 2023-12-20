eba041a5b6e4ed6a7db07145584085a6061f4570
MilanKorat35 <153885281+MilanKorat35@users.noreply.github.com>
2023-12-20 20:45:07 +0530

    Update urls.py

--- TASK-6/urls.py
+++ TASK-6/urls.py
@@ -1,1 +1,9 @@
+# students/urls.py
+from django.urls import path
+from .views import student_form, student_database
+
+urlpatterns = [
+    path('student_form/', student_form, name='student_form'),
+    path('student_database/', student_database, name='student_database'),
+]
 
