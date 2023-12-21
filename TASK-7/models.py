24cd23fb6ef55e8653de0f0b3bc8fd715384d060
MilanKorat35 <153885281+MilanKorat35@users.noreply.github.com>
2023-12-21 19:59:32 +0530

    Update models.py

--- TASK-7/models.py
+++ TASK-7/models.py
@@ -1,1 +1,12 @@
+# models.py
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    admission_date = models.DateField()
+    # Other fields as needed
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
