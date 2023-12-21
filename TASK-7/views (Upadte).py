14fb13758c400bc18e22e46db0c2ec25f5dd7a63
MilanKorat35 <153885281+MilanKorat35@users.noreply.github.com>
2023-12-21 19:59:19 +0530

    Update views (Upadte).py

--- TASK-7/views (Upadte).py
+++ TASK-7/views (Upadte).py
@@ -1,1 +1,16 @@
+# views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Student
+from .forms import StudentForm  # Assuming you have a form for the Student model
+
+def edit_student(request, student_id):
+    student = get_object_or_404(Student, id=student_id)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student_details', student_id=student.id)
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'edit_student.html', {'form': form, 'student': student})
 
