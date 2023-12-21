82a09e00bf79a51fa6f750549b183ae3786244df
MilanKorat35 <153885281+MilanKorat35@users.noreply.github.com>
2023-12-21 19:59:02 +0530

    Update views (Delete).py

--- TASK-7/views (Delete).py
+++ TASK-7/views (Delete).py
@@ -1,1 +1,11 @@
+# views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Student
+
+def delete_student(request, student_id):
+    student = get_object_or_404(Student, id=student_id)
+    if request.method == 'POST':
+        student.delete()
+        return redirect('home')  # Redirect to the home page or any other appropriate page
+    return render(request, 'delete_student.html', {'student': student})
 
