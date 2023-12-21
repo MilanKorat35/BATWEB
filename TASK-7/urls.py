4260b23fc04951ca37f3cfb4fff3fa06936e673a
MilanKorat35 <153885281+MilanKorat35@users.noreply.github.com>
2023-12-21 19:58:36 +0530

    Update urls.py

--- TASK-7/urls.py
+++ TASK-7/urls.py
@@ -1,1 +1,10 @@
+# urls.py
+from django.urls import path
+from .views import edit_student, delete_student
+
+urlpatterns = [
+    # Other patterns
+    path('edit/<int:student_id>/', edit_student, name='edit_student'),
+    path('delete/<int:student_id>/', delete_student, name='delete_student'),
+]
 
