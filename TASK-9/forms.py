3cfbf5983f0c12fac4656f9a03065ed3eabd411d
MilanKorat35 <153885281+MilanKorat35@users.noreply.github.com>
2023-12-26 19:06:34 +0530

    Create forms.py

---
+++ TASK-9/forms.py
@@ -0,0 +1,13 @@
+# blog/forms.py
+from django import forms
+from .models import Blog, Comment
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ['title', 'content', 'image']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
