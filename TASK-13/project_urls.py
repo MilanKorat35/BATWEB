506c66e6ce57337ffb82000e6da3151f314f5a2e
MilanKorat35 <153885281+MilanKorat35@users.noreply.github.com>
2023-12-31 16:16:25 +0530

    Update project_urls.py

--- TASK-13/project_urls.py
+++ TASK-13/project_urls.py
@@ -1,1 +1,9 @@
+# chatbot_project/urls.py
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('chatbot_app.urls')),
+]
 
